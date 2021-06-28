3c93cd330fa9b20df24a5fe4420741b355685630
cotton-r <66560099+cotton-r@users.noreply.github.com>
2021-06-28 22:56:11 +0100

    create InputField component and add it to ToDoList render method

---
+++ my-app/src/Components/ToDoList/ToDoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ToDoList.css';
+
+class ToDoList extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="InputField">
+                    {/* <InputField /> */}
+                </div>
+                <div className="itemList">
+                    
+                </div>
+            </div>
+        )
+    }
+}
